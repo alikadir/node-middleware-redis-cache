8086303d95dadbb00a8c51a517b8d2d062a3277b
Ali Kadir Bagcioglu <alikadirbagcioglu@gmail.com>
2020-10-14 18:55:14 +0300

    added local redis

--- index.js
+++ index.js
@@ -1,30 +1,47 @@
 import express from "express";
 import mung from "express-mung";
 import bodyParser from "body-parser";
+import redis from "redis";
 
 const app = express();
+const redisClient = redis.createClient();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
+// redis library does not support Promise build-in now (maybe v4) so we use callback "function (err, reply) { }"
+/*
+const { promisify } = require("util");
+const getAsync = promisify(client.get).bind(client);
+
+const value = await getAsync("key");
+*/
+
+// alternative npm i ioredis
+
+app.use(async (req, res, next) => {
   const cacheKey = `${req.path}|${JSON.stringify(req.body)}`;
-  console.log("======= Check to cache =======");
-  console.log({ cacheKey });
-  req.myCacheKey = cacheKey;
-  next();
+  redisClient.get(cacheKey, (err, cacheValue) => {
+    if (cacheValue) {
+      res.json(JSON.parse(cacheValue));
+    } else {
+      req.myCacheKey = cacheKey;
+      next();
+    }
+  });
 });
 
 app.use(
   mung.json((body, req, res) => {
-    console.log("======= Write to cache =======");
-    console.log(req.myCacheKey);
-    console.log(body);
+    redisClient.set(req.myCacheKey, JSON.stringify(body));
+    redisClient.expire(req.myCacheKey, 5); //TTL seconds
   })
 );
 
 app.all("/dummyendpoint/:id", (req, res) => {
-  res.json({ id: req.params.id, name: "alikadir" });
+  setTimeout(() => {
+    res.json({ id: req.params.id, name: "alikadir" });
+  }, 3000);
 });
 
 app.listen(1453, () => {
